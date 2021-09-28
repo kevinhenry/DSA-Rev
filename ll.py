542ce403127f73f2d85d3923a379d62b79181b35
Kevin Henry <kevinhenry@mac.com>
2021-09-27 20:06:16 -0700

    Initial Commit

---
+++ ll.py
@@ -0,0 +1,33 @@
+# ll = [2] -> [4] -> [6] -> None
+        ^
+# current ll.head
+
+# write a fuctnion that givine a linked list returne true if and false if 
+
+
+def sorted_ascending(ll):
+    if not ll.head:
+        return False
+    current = ll.head
+    while current.next:
+        if current.next.value < current.value:
+            return False
+        current = current.next
+    return True
+# Potential Solution Part 2
+
+def sorted_either_direction(ll):
+    if not ll.head:
+        return False
+    is_ascending = True
+    is_descending = True
+    current = ll.head
+    while current.next and (is_ascending or is_descending):
+        cv = current.value
+        nv = current.next.value
+        if nv < cv:
+            is_ascending = False
+        if nv > cv:
+            is_descending = False
+        current = current.next
+    return is_ascending or is_descending
